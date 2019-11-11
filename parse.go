86db1a7680dfe33a407ee00601c626f150e6ae51
ZeeK <46779447+imperialzhiyi@users.noreply.github.com>
2019-11-11 21:50:40 +0800

    Add files via upload

---
+++ parse.go
@@ -0,0 +1,41 @@
+package main
+
+import "regexp"
+
+type Config struct {
+	From          string
+	To            string
+	RedirectState string
+}
+
+var configRE = regexp.MustCompile(`Redirects?(\s+.*)`)
+var fromRE = regexp.MustCompile(`\s+from\s+(/\S*)`)
+var toRE = regexp.MustCompile(`\s+to\s+(https?\://\S+|/\S*)`)
+var stateRE = regexp.MustCompile(`\s+(permanently|temporarily)|\s+with\s+(301|302|307|308)`)
+
+func Parse(record string) *Config {
+	configMatches := configRE.FindStringSubmatch(record)
+	if len(configMatches) == 0 {
+		return nil
+	}
+
+	fromMatches := fromRE.FindStringSubmatch(configMatches[1])
+	toMatches := toRE.FindStringSubmatch(configMatches[1])
+	stateMatches := stateRE.FindStringSubmatch(configMatches[1])
+
+	config := new(Config)
+	if len(fromMatches) > 0 {
+		config.From = fromMatches[1]
+	}
+	if len(toMatches) > 0 {
+		config.To = toMatches[1]
+	}
+	if len(stateMatches) > 0 {
+		config.RedirectState = stateMatches[1]
+		if config.RedirectState == "" {
+			config.RedirectState = stateMatches[2]
+		}
+	}
+
+	return config
+}
---
+++ server.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+)
+
+func fallback(w http.ResponseWriter, r *http.Request, reason string) {
+	location := "http://redirect.name/"
+	if reason != "" {
+		location = fmt.Sprintf("%s#reason=%s", location, url.QueryEscape(reason))
+	}
+	http.Redirect(w, r, location, 302)
+}
+
+func getRedirect(txt []string, url string) (*Redirect, error) {
+	var catchAlls []*Config
+	for _, record := range txt {
+		config := Parse(record)
+		if config.From == "" {
+			catchAlls = append(catchAlls, config)
+			continue
+		}
+		redirect := Translate(url, config)
+		if redirect != nil {
+			return redirect, nil
+		}
+	}
+
+	var config *Config
+	for _, config = range catchAlls {
+		redirect := Translate(url, config)
+		if redirect != nil {
+			return redirect, nil
+		}
+	}
+
+	return nil, errors.New("No paths matched")
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	parts := strings.Split(r.Host, ":")
+	host := parts[0]
+
+	hostname := fmt.Sprintf("_redirect.%s", host)
+	txt, err := net.LookupTXT(hostname)
+	if err != nil {
+		fallback(w, r, fmt.Sprintf("Could not resolve hostname (%v)", err))
+		return
+	}
+
+	redirect, err := getRedirect(txt, r.URL.String())
+	if err != nil {
+		fallback(w, r, err.Error())
+	} else {
+		http.Redirect(w, r, redirect.Location, redirect.Status)
+	}
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" && len(os.Args) > 1 {
+		port = os.Args[1]
+	}
+	if port == "" {
+		port = "8081"
+	}
+	http.HandleFunc("/", handler)
+	srv := &http.Server{
+		Addr:         ":" + port,
+		ReadTimeout:  2 * time.Second,
+		WriteTimeout: 2 * time.Second,
+	}
+
+	log.Printf("Listening on http://127.0.0.1:%s", port)
+	log.Fatal(srv.ListenAndServe())
+}
